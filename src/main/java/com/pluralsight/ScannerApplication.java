100716f83126150c1d4e16f331da19736ab6e87a
Markeith Eley <meleynyc@gmail.com>
2025-09-26 12:27:18 -0400

    second push

---
+++ src/main/java/com/pluralsight/ScannerApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class ScannerApplication {
+    public static void main(String[] args) {
+
+        //set up the scanner y'all, we use this as our tool
+        // to capture input from the user
+        Scanner myScanner = new Scanner(System.in);
+    }
+}
